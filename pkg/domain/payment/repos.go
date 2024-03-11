ee1aa26e29584e4d6a716494634210de7d20ed22
Subba Reddy Veeramreddy <subbareddy.vrm@gmail.com>
2024-03-11 23:55:27 +0100

    feat: update domain

--- pkg/domain/payment/repos.go
+++ pkg/domain/payment/repos.go
@@ -3,9 +3,11 @@ package payment
 import (
 	"context"
 	"splitmoney/pkg/domain/account"
+	"time"
 )
 
 type Storer interface {
 	CreateTx(context.Context, Transaction) error
 	GetTxs(context.Context, account.AccountID) ([]Transaction, error)
+	GetTxsInDuration(_ context.Context, accID account.AccountID, from time.Time, to time.Time) ([]Transaction, error)
 }
--- pkg/domain/user/errors.go
+++ pkg/domain/user/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrorUserNotFound      = errors.New("user not found")
 	ErrorEmailAlreadyInUse = errors.New("email alraedy in use")
+	ErrorFailedToSignup    = errors.New("failed to signup")
 )
